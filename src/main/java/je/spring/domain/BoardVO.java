cc0d669bd61faeb3947c0bd7f9751a4ad0b8beb6
jieun <orbital0m0@gmail.com>
2021-02-22 14:21:32 +0900

    롬복 적용

--- src/main/java/je/spring/domain/BoardVO.java
+++ src/main/java/je/spring/domain/BoardVO.java
@@ -2,6 +2,13 @@ package je.spring.domain;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class BoardVO {
 	private int seq;
 	private String title;
@@ -9,42 +16,4 @@ public class BoardVO {
 	private String content;
 	private Date regDate = new Date();
 	private int cnt = 0;
-	public int getSeq() {
-		return seq;
-	}
-	public void setSeq(int seq) {
-		this.seq = seq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	}
-	
-	
 }
