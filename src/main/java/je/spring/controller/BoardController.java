1a6c1bf428afecd0a8cfec7f4e241af1d501a00f
jieun <orbital0m0@gmail.com>
2021-02-22 14:12:56 +0900

    컬렉션 리턴

--- src/main/java/je/spring/controller/BoardController.java
+++ src/main/java/je/spring/controller/BoardController.java
@@ -1,6 +1,8 @@
 package je.spring.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,22 @@ public class BoardController {
 		board.setRegDate(new Date());
 		board.setCnt(0);
 		return board;
+	}
+	
+	@GetMapping("/getBoardList")
+	public List<BoardVO> getBoardList(){
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		for(int i=1; i<=10; i++) {
+			BoardVO board = new BoardVO();
+			board.setSeq(1);
+			board.setTitle("테스트 제목");
+			board.setWriter("테스터");
+			board.setContent("테스트 내용");
+			board.setRegDate(new Date());
+			board.setCnt(0);
+			boardList.add(board);
+		}
+		return boardList;
 	}
 
 }
