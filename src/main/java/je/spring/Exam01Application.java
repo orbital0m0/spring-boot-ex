ecb76c6ca7c4cf4eb47389c19e2eb2c7a3b5ca64
jieun <orbital0m0@gmail.com>
2021-02-22 13:39:14 +0900

    웹 애플리케이션이 아닌 자바 애플리케이션으로 실행

--- src/main/java/je/spring/Exam01Application.java
+++ src/main/java/je/spring/Exam01Application.java
@@ -1,13 +1,17 @@
 package je.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Exam01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exam01Application.class, args);
+//		SpringApplication.run(Exam01Application.class, args);
+		SpringApplication application = new SpringApplication(Exam01Application.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 }
