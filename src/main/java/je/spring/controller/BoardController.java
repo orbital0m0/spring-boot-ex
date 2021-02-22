63b7c6f12f8a874aa29211e01329f9a196721d1e
jieun <orbital0m0@gmail.com>
2021-02-22 14:10:37 +0900

    RestController로 VO객체 리턴

--- src/main/java/je/spring/controller/BoardController.java
+++ src/main/java/je/spring/controller/BoardController.java
@@ -1,7 +1,11 @@
 package je.spring.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import je.spring.domain.BoardVO;
 
 @RestController
 public class BoardController {
@@ -13,6 +17,18 @@ public class BoardController {
 	@GetMapping("/hello")
 	public String hello(String name) {
 		return "Hello : " + name;
+	}
+	
+	@GetMapping("/getBoard")
+	public BoardVO getboard() {
+		BoardVO board = new BoardVO();
+		board.setSeq(1);
+		board.setTitle("테스트 제목");
+		board.setWriter("테스터");
+		board.setContent("테스트 내용");
+		board.setRegDate(new Date());
+		board.setCnt(0);
+		return board;
 	}
 
 }
